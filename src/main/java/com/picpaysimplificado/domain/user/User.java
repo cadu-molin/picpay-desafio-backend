88c0483301bb771217fcb6803cd4c6924c821e6d
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2024-09-15 18:46:23 -0300

    feat: add unit tests to UserRepository

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -35,4 +35,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
 }
--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -1,8 +1,19 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.mapstruct.user.UserMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +21,40 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
+    @Autowired
+    private UserRepository usuarioRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
-    void findUserByDocument() {
+    @DisplayName("Should get User successfully from DB")
+    void findUserByDocumentSuccessfully() {
+        String document = "12345678901";
+        UserDTO data = new UserDTO("Carlos", "Teste", document, new BigDecimal(10), "teste@gmail.com", "1234", UserType.COMMON);
+
+        createUser(data);
+
+        Optional<User> founbdedUser = usuarioRepository.findUserByDocument(document);
+
+        Assertions.assertTrue(founbdedUser.isPresent());
+    }
+
+    @Test
+    @DisplayName("Should not get User from DB when user not exists")
+    void findUserByDocumentUnsuccessful() {
+        String document = "12345678901";
+
+        Optional<User> founbdedUser = usuarioRepository.findUserByDocument(document);
+
+        Assertions.assertFalse(founbdedUser.isPresent());
+    }
+
+    private User createUser(UserDTO data) {
+        User newUser = new User(data);
+
+        entityManager.persist(newUser);
+
+        return newUser;
     }
 }
